e46129ffb5dd446fdf70ecc6f07eb231d7a85b58
itbz <hannes.forsgard@gmail.com>
2012-10-02 16:08:04 +0200

    Moved unit tests to subdir

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,5 @@
 <?php
 
 require_once __DIR__ . "/../vendor/autoload.php";
-require_once __DIR__ . "/DataMapper/Model.php";
-require_once __DIR__ . "/DataMapper/PDO-mysql/MysqlTestCase.php";
-
+require_once __DIR__ . "/unit/Model.php";
+require_once __DIR__ . "/unit/PDO-mysql/MysqlTestCase.php";
